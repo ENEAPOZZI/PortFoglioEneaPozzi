1afd34a3d75f603c6a89d2939d1b19606a52f372
EneaPozzi <eppsm2001@gmail.com>
2025-07-13 22:50:59 +0200

    Aggiunto : Favicon, pagina in lavorazione, prova pagina dei progetti

--- assets/pages/pagesassets/js/jspages.js
+++ assets/pages/pagesassets/js/jspages.js
@@ -1,20 +1,30 @@
+/* Per lo swiper */
 document.addEventListener("DOMContentLoaded", () => {
   const swipertech = new Swiper(".swipertech", {
     loop: true,
     spaceBetween: 20,
     centeredSlides: true,
-    speed: 4000,
-    autoplay: {
-      delay: 0,
-      disableOnInteraction: false,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
     },
     breakpoints: {
       0: {
-        slidesPerView: 1,
+        slidesPerView: 3,     
+        centeredSlides: true
       },
       768: {
-        slidesPerView: 3,
+        slidesPerView: 3,     
+        centeredSlides: true
       }
     }
   });
+});
+
+/* Per gestire le animazioni*/
+AOS.init({
+  duration: 1000,     
+  once: true,         
+  offset: 120,        
+  easing: 'ease-in-out'
 });
